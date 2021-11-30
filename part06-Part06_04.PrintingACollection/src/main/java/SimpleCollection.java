5448a8ed614c2a57e83f200290ec993a271e15d7
GuaBaiChi <87482626+GuaBaiChi@users.noreply.github.com>
2021-11-30 16:35:06 -0500

    2021.11.30 afternoon

--- part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
+++ part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
@@ -5,18 +5,42 @@ public class SimpleCollection {
 
     private String name;
     private ArrayList<String> elements;
+    private int count;
 
     public SimpleCollection(String name) {
         this.name = name;
         this.elements = new ArrayList<>();
+        this.count = count;
+    }
+    
+    public int getCount() {
+        return this.count;
+    }
+    
+    public String getName() {
+        return this.name;
     }
 
     public void add(String element) {
         this.elements.add(element);
+        this.count++;
     }
 
     public ArrayList<String> getElements() {
         return this.elements;
     }
     
+        public String toString() {
+        if (this.elements.isEmpty()) {
+            return "The collection " + this.name + " is empty.";
+        } 
+        else if (this.elements.size() == 1) {
+            return "The collection " + this.name + " has " + this.elements.size() + " element: " + this.elements;
+        }
+        else {
+            return "The collection " + this.name + " has " + this.elements.size() + " elements: " + this.elements;
+        }
+    }
 }
+
+
